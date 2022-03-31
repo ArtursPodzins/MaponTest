33675ab4436f36cbbe419a421b570e866ab8e1d3
Arturs <arturs.podzins@mapon.com>
2022-03-31 14:43:09 +0300

    Db Config

--- classes/dbh.classes.php
+++ classes/dbh.classes.php
@@ -1,14 +1,18 @@
 <?php
 
 class Dbh {
-
-    private $host = "host=eu-cdbr-west-02.cleardb.net";
-    private $user = "b71f20df94555f";
-    private $pwd = "8527f4eb";
-    private $dbname = "heroku_d5d58245fb0f454";
+    
     protected function connect(){
-        $dsn = 'mysql:host='. $this->host . ';dbname=' . $this->dbname;
-        $pdo = new PDO($dsn, $this->user, $this->pwd);
+        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $active_group = 'default';
+        $query_builder = TRUE;
+        $host = $cleardb_url["host"];
+        $user = $cleardb_url["user"];
+        $pwd = $cleardb_url["pass"];
+        $dbname = "heroku_d5d58245fb0f454";
+        $dsn = 'mysql:host='. $host . ';dbname=' . $dbname;
+        $pdo = new PDO($dsn, $user, $pwd);
         return $pdo;
     }
 }
+
