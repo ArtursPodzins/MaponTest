b03ded90d7423539248790b31cfd9e08fd05f2cc
Arturs <arturs.podzins@mapon.com>
2022-03-31 14:52:09 +0300

    DB conf

--- classes/dbh.classes.php
+++ classes/dbh.classes.php
@@ -4,14 +4,12 @@ class Dbh {
     
     protected function connect(){
         $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $active_group = 'default';
-        $query_builder = TRUE;
         $host = $cleardb_url["host"];
         $user = $cleardb_url["user"];
         $pwd = $cleardb_url["pass"];
         $dbname = "heroku_d5d58245fb0f454";
-        $dsn = 'mysql:host='. $host . ';dbname=' . $dbname;
-        $pdo = new PDO($dsn, $user, $pwd);
+
+        $pdo = new PDO("mysql:host=' . $host.'; dbname=' . $dbname.';", $user, $pwd);
         return $pdo;
     }
 }
