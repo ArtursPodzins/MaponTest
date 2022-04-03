8df432eeff24094a1da100e091842b1bb31bc732
Arturs <arturs.podzins@mapon.com>
2022-04-03 22:35:51 +0300

    Bug

--- classes/login.classes.php
+++ classes/login.classes.php
@@ -7,7 +7,7 @@ class Login extends Dbh{
         // Selecting password from the username that user gave
         $stmt = $this->connect()->prepare('SELECT users_pwd FROM users WHERE users_uid = ?;');
         print_r($stmt);
-        header("location: ../index.php?error=stmtfailed");
+        header("location: login.classes.php?error=stmtfailed");
             exit();
 
         // Checking if the statement executed
